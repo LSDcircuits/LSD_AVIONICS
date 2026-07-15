d85566eea9e10ea40b748b4c53b3cf47e09a199f
Lorenzo Salvatore Daidone <116204425+LSDcircuits@users.noreply.github.com>
2026-07-15 18:57:19 +0200

    Enhance comments in 1D Kalman filter implementation
    Updated comments for clarity on Kalman filter parameters and calculations.

--- Algorithims/1D_kalman_example.c
+++ Algorithims/1D_kalman_example.c
@@ -1,21 +1,25 @@
 typedef struct {
-    float h;      // altitude estimate
-    float P;      // variance
-    float Q;      // process noise
-    float R;      // barometer noise
+    float h;      // altitude estimate <- this is the value used for height (never 100% certain so called estimate)
+    float P;      // variance <- initial estimate
+    float Q;      // process noise 
+    float R;      // barometer noise from Data sheet, how wrong could the sensor be
 } KF1D;
 
 void kf_update(KF1D *kf, float z) {
     // Prediction step
-    // theres a known variance and a noise created by the program which can be derived,
-    // these together give the prediction spread. 
+    // P = P0 from first loop is a initial estimate & P_pred is the new estimate with added Process noise
     float P_pred = kf->P + kf->Q;
     
     // Update
-    float y = z - kf->h;
-    float S = P_pred + kf->R;
-    float K = P_pred / S;
+    float y = z - kf->h; // This gives a difference between measured value and (prediction "last filtered measurement"), 
+    // so if y = 0 kalman gain (K) multiplies by 0 giving 100% confidence. The reason Y is importantis it gives a values between the 
+    // last measured height estimate to the current sensor measurement if the difference is big over cycles it corrects it self to a smaller error. 
     
-    kf->h = kf->h + K * y;
-    kf->P = (1.0f - K) * P_pred;
+    float S = P_pred + kf->R; // 
+    float K = P_pred / S;     // these two make K = P_pred / (P_red + R) <- Kallman gain equation
+    // the Kalman gain equation is a value between 0 and 1 which represent how confidence it is about the sensor reading given the noise of the sensor,
+    // when the noise is very high the denominator dominates bringing the K value down. 
+    
+    kf->h = kf->h + K * y; // update step 
+    kf->P = (1.0f - K) * P_pred; // 
 }
